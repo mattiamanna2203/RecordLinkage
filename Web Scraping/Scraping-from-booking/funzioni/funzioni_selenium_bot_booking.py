eff74d27e8e6a207ecd0f09f6cd5ca8911e8a402
mattiamanna2203 <mattiamanna2203@gmail.com>
2025-07-04 17:03:55 +0200

    Update scraping booking
    Aggiunti commenti scraper per booking

--- Web Scraping/Scraping-from-booking/funzioni/funzioni_selenium_bot_booking.py
+++ Web Scraping/Scraping-from-booking/funzioni/funzioni_selenium_bot_booking.py
@@ -6,12 +6,17 @@ from selenium.webdriver.common.by import By
 # Funzioni utilizzate dal bot selenium.
 def click_cookie_button(driver):
     """
-    Questa funzione si occupa di accettare i cookies quando si accede su booking
+    Questa funzione accetta i cookies quando si accede a booking.
+    In input prende un selenium driver.
+    Sono proposti 2 metodi per accettare i cookie in modo da avere meno possibilità di errore.
+    Quando i cookie sono accettati con un metodo qualsiasi la funzione si interrompe.
+    
+    I metodi per accettare i cookie sono incapsulati tra dei TRY & EXCEPT in modo da evitare l'interruzione della funzione in caso di errore per un metodo, così da continuare con il metodo successivo.
     """
     try:
         # Metodo di accettazione 1
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Accetto']"))).click()
-        print("Cookie accettati metodo 1")
+        print("Cookie accettati con metodo 1")
         return 
     except:
         pass
@@ -19,7 +24,7 @@ def click_cookie_button(driver):
     try:
         # Metodo di  accettazione 2
         WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"button#onetrust-accept-btn-handler"))).click()
-        print("Cookie accettati metodo 2")
+        print("Cookie accettati con metodo 2")
         return 
     except:
         pass
@@ -30,12 +35,18 @@ def click_cookie_button(driver):
 # Funzioni utilizzate dal bot selenium.
 def click_caricaaltri_button(driver):
     """
-    Questa funzione si occupa di accettare i cookies quando si accede su booking
-    """
+    Questa funzione si occupa di caricare altri risultati cliccando sul pulsante apposito.
+    In input prende un selenium driver.
+    
+    Sono proposti 3 metodi per caricare altri risultati in modo da avere meno possibilità di errore.
+    Quando si riesce a caricare altri risultati la funzione si interrompe.
+    
+    I metodi sono incapsulati tra dei TRY & EXCEPT in modo da evitare l'interruzione della funzione in caso di errore per un metodo, così da continuare con il metodo successivo.
+    """   
     try:
         # Metodo di accettazione 1
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Carica più risultati']"))).click()
-        print("Carica altri risultati metodo 1")
+        print("Carica altri risultati con metodo 1")
         return 
     except:
         pass
@@ -43,7 +54,7 @@ def click_caricaaltri_button(driver):
     try:
         # Metodo di  accettazione 2
         WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.de576f5064 b46cd7aad7 d0a01e3d83 dda427e6b5 bbf83acb81 a0ddd706cc"))).click()
-        print("Carica altri risultati metodo 2")
+        print("Carica altri risultati con metodo 2")
         return 
     except:
         pass
@@ -51,7 +62,7 @@ def click_caricaaltri_button(driver):
     try:
         # Metodo di  accettazione 3
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Carica più risultati']"))).click()
-        print("Carica altri risultati metodo 3")
+        print("Carica altri risultati con metodo 3")
         return 
     except:
         pass
